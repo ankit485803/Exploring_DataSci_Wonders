7f2f93e114c0015b1fdbdea98e6e17e503dc3e6e
Ankit Kumar <ankit485803@gmail.com>
2026-04-26 21:08:15 +0530

    Add Numerical Methods course metadata
    Winter Sem 2 IIT Patna (CDA106, Prof. NK Tomar)

---
+++ Numerical_MethodsFOR__DataSc/aboutCoursework_106.m
@@ -0,0 +1,11 @@
+
+// 27th Jan 2024 (Saturday - after Repulbic day and before IIIT Guwahati internship & ISRO - NESAC IIRS) winter Sem2 IIT Patna
+
+BO CDA 106 Numerical methods for Data Science 
+
+by Prof NK Tomar Sir and Kuldeep Singh Patel
+
+Labs session on MatLab and its license from institute email id ankit127
+
+
+
